874e141f71dc0493d13795e538493110084558c0
eugengh <70998165+eugengh@users.noreply.github.com>
2021-11-13 14:07:40 +0200

    Update pr8pag60.py

--- pr8pag60.py
+++ pr8pag60.py
@@ -6,3 +6,4 @@ print("Intersectia acestor doua multimi este: ", s1.intersection(s2))
 print("Reuniunea acestor doua multimi este: ", s1.union(s2))
 print("Diferenta s1\s2 este: ", s1.difference(s2))
 print("Diferenta s2\s1 este: ", s2.difference(s1))
+#problema 8,9
