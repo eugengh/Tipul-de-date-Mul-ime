0991d055f6d44c52865a1c44928561c672aa3988
eugengh <70998165+eugengh@users.noreply.github.com>
2021-11-13 14:06:10 +0200

    Add files via upload

---
+++ pr10pag60.py
@@ -0,0 +1,16 @@
+import itertools 
+s = {1,2,3,4}
+data2 = itertools.combinations(s, 2)
+data1 = itertools.combinations(s, 1)
+data3 = itertools.combinations(s, 3)
+data4 = itertools.combinations(s,4)
+subsets = set(data1)
+subsets1 = set(data2)
+subsets2 = set(data3)
+subsets3 = set(data4)
+a = subsets.union(subsets1)
+b = subsets2.union(subsets3)
+c = list(a.union(b))
+y = sorted(c)
+print(y)
+
---
+++ pr8pag60.py
@@ -0,0 +1,8 @@
+s1 = set(eval(input("Introdu multimea: ")))
+s2 = set(eval(input("Introdu a doua multime: ")))
+print(s1)
+print(s2)
+print("Intersectia acestor doua multimi este: ", s1.intersection(s2))
+print("Reuniunea acestor doua multimi este: ", s1.union(s2))
+print("Diferenta s1\s2 este: ", s1.difference(s2))
+print("Diferenta s2\s1 este: ", s2.difference(s1))
